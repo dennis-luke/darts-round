91d73ddf35a827826f8733d66d24ae65c465320f
Luke Dennis <luke.dennis@canatab.net>
2026-04-07 22:23:27 +0100

    Add subtle full screen button so that scoreboard can be made full screen, even on macs

--- src/app/scoreboard/scoreboard.component.ts
+++ src/app/scoreboard/scoreboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { TeamBoardComponent } from '../team-board/team-board.component';
@@ -13,9 +13,27 @@ import { GameState, Team } from '../models';
 })
 export class ScoreboardComponent implements OnInit {
   gameState: GameState = this.initGamesState();
+  isFullscreen = false;
+
+  constructor(private elementRef: ElementRef) {}
 
   ngOnInit() {
     this.gameState = this.initGamesState();
+  }
+
+  toggleFullscreen(): void {
+    if (this.isFullscreen) {
+      document.exitFullscreen?.() || (document as any).webkitExitFullscreen?.();
+    } else {
+      const el = this.elementRef.nativeElement;
+      el.requestFullscreen?.() || (el as any).webkitRequestFullscreen?.();
+    }
+  }
+
+  @HostListener('document:fullscreenchange')
+  @HostListener('document:webkitfullscreenchange')
+  onFullscreenChange(): void {
+    this.isFullscreen = !!document.fullscreenElement || !!(document as any).webkitFullscreenElement;
   }
 
   initGamesState(): GameState {
